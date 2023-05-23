d5a9f04ce82d25d4c12a355012f11ad07ff170f0
Nicolas Bissig <nicolas.bissig@device-insight.com>
2023-05-23 07:35:00 +0000

    feat: clean line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,53 @@
+use std::{str::FromStr, num::ParseFloatError};
+
 #[derive(Debug)]
 struct Point {
     x: f64,
     y: f64
 }
 
+impl Point {
+    fn from_str(x: &str, y: &str) -> Result<Point, ParseFloatError> {
+        let p = Point {
+            x: x.parse()?,
+            y: y.parse()?,
+        };
+
+        return Ok(p);
+    }
+}
+
 #[derive(Debug)]
 struct Line {
     p: Point,
     q: Point
+}
+
+enum ParseLineError {
+    ParseFloat(ParseFloatError),
+    NotFourElements
+}
+
+impl FromStr for Line {
+    type Err = ParseLineError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let splits: Vec<_> = s.split(" ").collect();
+
+        if splits.len() != 4 {
+            return Err(ParseLineError::NotFourElements)
+        }
+
+        let p = Point::from_str(&splits[0], &splits[1]).map_err(|e| ParseLineError::ParseFloat(e))?;
+        let q = Point::from_str(&splits[2], &splits[3]).map_err(|e| ParseLineError::ParseFloat(e))?;
+
+        let line = Line {
+            p,
+            q,
+        };
+
+        return Ok(line);
+    }
 }
 
 fn main() {
