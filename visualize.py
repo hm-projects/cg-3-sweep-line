05999325b98422e2c7bf4a264c96ef534c2ae955
NicolasBissig <nicbissig@gmail.com>
2023-07-14 13:57:59 +0200

    feat: better viz

--- visualize.py
+++ visualize.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
+import sys
+
+LINE_WIDTH = 0.25
+POINT_R = 0.5
+
+# get the filename from first argument
+filename = sys.argv[1]
 
 fig, ax = plt.subplots()
 
 # Read the file with lines coordinates
-lines_filename = "data/s_1000_10.dat"
-with open(lines_filename, "r") as file:
+with open(filename, "r") as file:
     lines = file.readlines()
 
 # Extract the line coordinates
@@ -19,10 +25,10 @@ for line in lines:
 for start, end in line_coordinates:
     x = [start[0], end[0]]
     y = [start[1], end[1]]
-    ax.plot(x, y, 'b', linewidth = 0.1)
+    ax.plot(x, y, 'b', linewidth=LINE_WIDTH)
 
 # Read the file with additional points
-points_filename = "data/s_1000_10.dat.i"
+points_filename = f"{filename}.i"
 with open(points_filename, "r") as file:
     points = file.readlines()
 
@@ -37,12 +43,14 @@ for point in points:
 # Plot the points
 x_points = [coord[0] for coord in point_coordinates]
 y_points = [coord[1] for coord in point_coordinates]
-ax.scatter(x_points, y_points, color='r', marker='o', s=0.05)
+ax.scatter(x_points, y_points, color='r', marker='o', s=POINT_R)
 
 # Set the plot title and labels
-ax.set_title("Lines and Points Plot")
-ax.set_xlabel("X-axis")
-ax.set_ylabel("Y-axis")
+ax.set_title("Line segment intersections (Sweep-line algorithm))")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
 
 fig.set_dpi(300)
+fig.tight_layout()
+fig.savefig(f"{filename}.svg", dpi=2000)
 plt.show()
